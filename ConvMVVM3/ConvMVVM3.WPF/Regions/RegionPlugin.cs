f9a1f64beaa47bc55e61b4e321001c62d2068921
BongHoe Koo <42884297+gellston@users.noreply.github.com>
2026-02-08 19:42:56 +0900

    1. Region 예외처리 업데이트

--- ConvMVVM3/ConvMVVM3.WPF/Regions/RegionPlugin.cs
+++ ConvMVVM3/ConvMVVM3.WPF/Regions/RegionPlugin.cs
@@ -30,48 +30,58 @@ namespace ConvMVVM3.WPF.Regions
         #region Event Handler
         public static void OnRegionNameChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
 
-            if (!(d is FrameworkElement fe))
-                return;
 
-            var name = e.NewValue as string;
-            if (string.IsNullOrEmpty(name)) return;
-
-            var regionManager = ServiceLocator.GetService<IRegionManager>();
-            var behaivorCollection = Interaction.GetBehaviors(d);
-
-            if (d is ContentControl content)
+            try
             {
-                regionManager.RegisterViewWithRegion(name, RegionType.SingleView);
-                var region = regionManager.Regions[name];
-                var contentControlBehaivor = new ContentControlBehavior();
-                contentControlBehaivor.CurrentRegion = (Region)region;
-                contentControlBehaivor.CurrentRegion.IsAttaced = true;
-                behaivorCollection.Add(contentControlBehaivor);
-                return;
-            }
+                if (!(d is FrameworkElement fe))
+                    return;
+
+                var name = e.NewValue as string;
+                if (string.IsNullOrEmpty(name)) return;
+
+                var regionManager = ServiceLocator.GetService<IRegionManager>();
+                var behaivorCollection = Interaction.GetBehaviors(d);
+
+                if (d is ContentControl content)
+                {
+                    regionManager.RegisterViewWithRegion(name, RegionType.SingleView);
+                    var region = regionManager.Regions[name];
+                    var contentControlBehaivor = new ContentControlBehavior();
+                    contentControlBehaivor.CurrentRegion = (Region)region;
+                    contentControlBehaivor.CurrentRegion.IsAttaced = true;
+                    behaivorCollection.Add(contentControlBehaivor);
+                    return;
+                }
 
 
-            if (d is Selector selector)
-            {
-                regionManager.RegisterViewWithRegion(name, RegionType.MultiView);
-                var region = regionManager.Regions[name];
-                var selectorBehavior = new SelectorBehaivor();
-                selectorBehavior.CurrentRegion = (Region)region;
-                selectorBehavior.CurrentRegion.IsAttaced = true;
-                behaivorCollection.Add(selectorBehavior);
-                return;
-            }
+                if (d is Selector selector)
+                {
+                    regionManager.RegisterViewWithRegion(name, RegionType.MultiView);
+                    var region = regionManager.Regions[name];
+                    var selectorBehavior = new SelectorBehaivor();
+                    selectorBehavior.CurrentRegion = (Region)region;
+                    selectorBehavior.CurrentRegion.IsAttaced = true;
+                    behaivorCollection.Add(selectorBehavior);
+                    return;
+                }
 
-            if (d is ItemsControl itemsControl)
-            {
-                regionManager.RegisterViewWithRegion(name, RegionType.MultiView);
-                var region = regionManager.Regions[name];
-                var itemsControlBehavior = new ItemsControlBehavior();
-                itemsControlBehavior.CurrentRegion = (Region)region;
-                itemsControlBehavior.CurrentRegion.IsAttaced = true;
-                behaivorCollection.Add(itemsControlBehavior);
-                return;
+                if (d is ItemsControl itemsControl)
+                {
+                    regionManager.RegisterViewWithRegion(name, RegionType.MultiView);
+                    var region = regionManager.Regions[name];
+                    var itemsControlBehavior = new ItemsControlBehavior();
+                    itemsControlBehavior.CurrentRegion = (Region)region;
+                    itemsControlBehavior.CurrentRegion.IsAttaced = true;
+                    behaivorCollection.Add(itemsControlBehavior);
+                    return;
+                }
+
             }
+            catch { 
+            
+            
+            }
+            
 
         }
         #endregion
